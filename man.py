6b439328203ae266178a48d96427b937bd7b1b79
nu20 <bibhutipatra123@gmail.com>
2024-10-10 18:13:34 +0530

    first commit

---
+++ man.py
@@ -0,0 +1,23 @@
+def sub( a , b ) :
+    dac = a-b
+    print (dac)
+
+sub(5,5)
+    
+def add( a , b ) :
+    sum = a + b
+    print (sum)
+
+add(4,8)
+
+def mag( a , b ) :
+    mul = a * b
+    print (mul)
+
+mag(2,8)
+
+def divide ( a , b ) :
+    div = a / b
+    print (div)
+
+divide(4,9)
